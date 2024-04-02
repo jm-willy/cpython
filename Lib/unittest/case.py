8e9c045498e5cf21e77ff1a1e81d381a89c5e999
jm-willy <53955471+jm-willy@users.noreply.github.com>
2024-04-02 12:58:03 +0200

    Update case.py

--- Lib/unittest/case.py
+++ Lib/unittest/case.py
@@ -887,8 +887,8 @@ class TestCase(object):
            difference rounded to the given number of decimal places
            (default 7) and comparing to zero, or by comparing that the
            difference between the two objects is more than the given
-           delta and by comparing that the first object is greater 
-           than the second object plus delta and smaller than the 
+           delta, or by comparing that the first object is greater 
+           than the second object plus delta or smaller than the 
            second object minus detla.
 
            Note that decimal places (from zero) are usually not the same
@@ -941,8 +941,8 @@ class TestCase(object):
            difference rounded to the given number of decimal places
            (default 7) and comparing to zero, or by comparing that the
            difference between the two objects is less than the given
-           delta and by comparing that the first object is not greater 
-           than the second object plus delta and not smaller than the 
+           delta, or by comparing that the first object is not greater 
+           than the second object plus delta or not smaller than the 
            second object minus delta.
 
            Note that decimal places (from zero) are usually not the same
